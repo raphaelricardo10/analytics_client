abddc54281b1e4a416150fea4353f8544fc2e77b
Raphael <raphaelricardo10@gmail.com>
2022-12-07 20:08:25 -0300

    refactor: removed the dependency of dataframe. It must be preprocessed before calling the plot method

--- plots/temperature_plot.py
+++ plots/temperature_plot.py
@@ -1,19 +1,15 @@
-import pandas as pd
 from plots.plot import Plot
 import plotly.express as px
 import plotly.graph_objects as go
-
 class TemperaturePlot(Plot):
     def figure(self, df) -> go.Figure:
-        df = pd.read_json(df)
-        df['ts'] = pd.to_datetime(df['ts'], unit='ms')
         figs = self.plot_all_devices(df)
 
         return self.join_plots(figs)
 
     def plot_all_devices(self, df):
         figs = []
-        devices = df['device'].unique()
+        devices = df.device.unique()
 
         for device in devices:
             df_by_device = df[df.device == device]
