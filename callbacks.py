c66e158221b64d78f44ca436c47d747d2ebb89c8
Raphael <raphaelricardo10@gmail.com>
2022-12-08 00:17:38 -0300

    feat: added caching to df update because of long-running queries. Added plot of temperature by hour

--- callbacks.py
+++ callbacks.py
@@ -1,39 +1,45 @@
 import os
 import pandas as pd
 
+import diskcache
+from dash.long_callback import DiskcacheLongCallbackManager
+
 from app import app
 from dash import Output, Input, State, no_update
 
 from decoders import decode_dataframe
 from bigquery_client import BigQueryClient
-from plots.temperature_plot import TemperaturePlot
+from plots.temperature_plots import LastHourTemperaturePlot
 
-temperature_plot = TemperaturePlot()
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
 
 bq_client = BigQueryClient(
     project_id=os.getenv("PROJECT_ID"), dataset=os.getenv("BQ_DATASET")
 )
 
 
-@app.callback(
+@app.long_callback(
     Output(component_id="df", component_property="data"),
-    Output(component_id='df_start_date', component_property='data'),
+    Output(component_id="df_start_date", component_property="data"),
     State(component_id="df", component_property="data"),
-    State(component_id='df_start_date', component_property='data'),
+    State(component_id="df_start_date", component_property="data"),
     Input(component_id="interval-component", component_property="n_intervals"),
+    manager=long_callback_manager,
 )
 def update_df(df, start_date, _):
     df = decode_dataframe(df)
-    
+    limit = 500 if df is not None else None
+
     new_df = bq_client.select(
-        "multisensor_data", start_date=start_date, to_df=True
+        "multisensor_data", start_date=start_date, to_df=True, limit=limit
     ).drop_duplicates()
 
     start_date = new_df.ts.max()
 
     if df is None:
         return new_df.to_json(), start_date
-        
+
     df = pd.concat([df, new_df], join="inner", ignore_index=True)
 
     return df.to_json(), start_date
@@ -48,4 +54,16 @@ def update_temperature_plot(df):
         return no_update
 
     df = decode_dataframe(df)
-    return temperature_plot.figure(df)
+    return LastHourTemperaturePlot.figure(df)
+
+
+@app.callback(
+    Output(component_id="interval-component", component_property="interval"),
+    Input(component_id="interval-component", component_property="n_intervals"),
+)
+def update_interval(num_intervals):
+    if num_intervals > 0:
+        return 2*1000
+
+    return no_update
+
