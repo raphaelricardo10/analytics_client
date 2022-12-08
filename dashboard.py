3fbfacde4ee440e468f0c3c5d407fc770075451e
Raphael <raphaelricardo10@gmail.com>
2022-12-08 06:10:52 -0300

    feat: added some styles to dashboard

--- dashboard.py
+++ dashboard.py
@@ -3,20 +3,29 @@ from dash import html, dcc
 
 from plots.plot import Plot
 
-app.layout = html.Div(children=[
-    dcc.Store('df'),
-    dcc.Store('df_start_date', data=None),
-    html.H1(children='Hello Dash'),
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-    dcc.Graph(
-        id='temperature-plot',
-        figure=Plot.empty_plot()
-    ),
-    dcc.Interval(
-        id='interval-component',
-        interval=10*1000, # in milliseconds
-        n_intervals=0,
-    )
-])
+app.layout = html.Div(
+    children=[
+        dcc.Store("df"),
+        dcc.Store("df_start_date", data=None),
+        html.Div(
+            className='titles-container',
+            children=[
+                html.H1(
+                    children="Real-time monitoring dashboard", className="page-title"
+                ),
+                html.H2(
+                    children="Easily keep track of sensors no matter where they are",
+                    className="page-subtitle",
+                ),
+            ]
+        ),
+        dcc.Graph(
+            id="temperature-plot", figure=Plot.empty_plot(), className="temp-plot-hour"
+        ),
+        dcc.Interval(
+            id="interval-component",
+            interval=10 * 1000,  # in milliseconds
+            n_intervals=0,
+        ),
+    ]
+)
